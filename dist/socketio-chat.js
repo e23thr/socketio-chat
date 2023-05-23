3899aa65c462828aecaa61cb5c2cb5706ff95ad3
Praphan Oranphanlert <e23thr@gmail.com>
2023-05-23 19:28:36 +0700

    fix: umd js

--- dist/socketio-chat.js
+++ dist/socketio-chat.js
@@ -1,36 +1,28 @@
-const n = "auth_token", e = "join", t = "message", s = "upload", i = "advertise";
-function f(o) {
-  return o.type === e && typeof o.room == "string" && !!o.room;
-}
-function c(o) {
-  return o.type === t && typeof o.message == "string" && !!o.message && typeof o.room == "string" && !!o.room;
+const e = "auth_token", n = "join", t = "message", s = "upload", r = "advertise";
+function i(o) {
+  return o.type === n && typeof o.room == "string" && !!o.room;
 }
 function E(o) {
+  return o.type === t && typeof o.message == "string" && !!o.message && typeof o.room == "string" && !!o.room;
+}
+function f(o) {
   return o.type === s && o.file instanceof File && typeof o.room == "string" && !!o.room;
 }
 function u(o) {
-  return o.type === n && typeof o.token == "string";
+  return o.type === e && typeof o.token == "string";
 }
-function g(o) {
-  return o.type === i && typeof o.json == "string" && !!o.json;
+function c(o) {
+  return o.type === r && typeof o.json == "string" && !!o.json;
 }
-class r {
-  constructor() {
-    console.log("Chat");
-  }
-}
-typeof window < "u" && (console.log("window is defined"), window.addEventListener("load", () => {
-  window.ChatClient = new r();
-}));
 export {
-  i as EVENT_ADVERTISE,
-  n as EVENT_AUTH_TOKEN,
-  e as EVENT_JOIN,
+  r as EVENT_ADVERTISE,
+  e as EVENT_AUTH_TOKEN,
+  n as EVENT_JOIN,
   t as EVENT_MESSAGE,
   s as EVENT_UPLOAD,
-  g as isAdvertisePayload,
+  c as isAdvertisePayload,
   u as isAuthTokenPayload,
-  f as isJoinPayload,
-  c as isMessagePayload,
-  E as isUploadPayload
+  i as isJoinPayload,
+  E as isMessagePayload,
+  f as isUploadPayload
 };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
 export * from "./types";
 export * from "./payload";
 export * from "./helpers";
-
-class Chat {
-  constructor() {
-    console.log("Chat");
-  }
-}
-
-if (typeof window !== "undefined") {
-  console.log("window is defined");
-  window.addEventListener("load", () => {
-    (window as any).ChatClient = new Chat();
-  });
-}
